97876a53781c10254fb42e60d932e88e9161363e
kaefish <kaefish@umich.edu>
2020-11-15 16:31:46 -0500

    correct consoles

--- js/video.js
+++ js/video.js
@@ -18,28 +18,31 @@ document.querySelector("#pause").addEventListener("click", function() {
 });
 
 document.querySelector("#volumeSlider").addEventListener("change", function () {
-	console.log(this);
-	console.log(this.value);
 	let vol = document.querySelector("#volume").innerHTML = this.value + "%";
 	video.volume = this.value / 100;
+	console.log(video.volume)
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
-	console.log("Slower");
-	video.playbackRate -= .1;
-	console.log(video.playbackRate);
+	video.playbackRate -= .1 * video.playbackRate;
+	console.log("New speed is " + video.playbackRate);
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
-	console.log("Faster");
-	video.playbackRate += .1;
-	console.log(video.playbackRate);
+	video.playbackRate += .1 * video.playbackRate;
+	console.log("New speed is " + video.playbackRate);
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
-	console.log("Skip");
-	video.currentTime += 5;
-	console.log(video.currentTime);
+	if (video.duration <= video.currentTime + 5){
+		console.log("Going back to beginning")
+		video.currentTime = 0
+	}
+	else{
+		video.currentTime += 5;
+	}
+	console.log(video.duration, 5)
+	console.log("Current Location is " + video.currentTime);
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
@@ -47,12 +50,10 @@ document.querySelector("#mute").addEventListener("click", function() {
 	if (video.muted == false){
 		video.muted = true;
 		let mute = document.querySelector("#mute").innerHTML = "Unmute";
-		console.log("Muted");
 	}
 	else {
 		video.muted = false;
 		let mute = document.querySelector("#mute").innerHTML = "Mute"; 
-		console.log("Unmuted");
 	}
 });
 
